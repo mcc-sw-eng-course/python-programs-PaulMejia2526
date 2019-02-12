ceae57b23fc6fba510a733ba039ae954a825d1a7
Nataly Alonso <T3C-abc-123>
2019-02-12 01:45:58 -0600

    L4 Updates

--- L4/myStatistics.py
+++ L4/myStatistics.py
@@ -45,17 +45,19 @@ class myStatistics:
         list.sort()
         n = len(list)
         if n % 2 == 0 and n >= 2:
-            median = (list[n / 2] + list[n / 2 + 1]) / 2
+            i = int(n / 2)
+            median = (list[i] + list[i - 1]) / 2
         elif n % 2 == 1 and n >= 2:
             median = list[int(n / 2)]
         elif n == 1:
-            median = list[n]
+            median = list[n-1]
         else:
             return
         return median
 
     def getNquartil(self, n, list):
-        list.sort
+        list.sort()
+
         l = len(list)
         odd = l % 2 == 1
         quartil = 0
@@ -66,11 +68,15 @@ class myStatistics:
             if not qi.is_integer():
                 i = int(qi) - 1
                 d = qi - int(qi)
-                qi = list[i] + d * (list[i + 1] - list[i])
-        return round(qi)
+                qi = list[i] + d * (list[i+1] - list[i])
+
+            else:
+                qi = list[int(qi-1)]
+
+        return qi
 
     def getNpercentil(self, n, list):
-        list.sort
+        list.sort()
         l = len(list)
         odd = l % 2 == 1
         quartil = 0
@@ -82,4 +88,9 @@ class myStatistics:
                 i = int(pi) - 1
                 d = pi - int(pi)
                 pi = list[i] + d * (list[i + 1] - list[i])
-        return round(pi)
+
+            else:
+                pi = list[int(pi - 1)]
+
+
+        return pi
--- L4/unittestStatistics.py
+++ L4/unittestStatistics.py
@@ -20,6 +20,75 @@ class TestStatistics(unittest.TestCase):
        result = handlerStatistics.getStandarDeviation(myList)
        self.assertEqual(result, 15.510365197874245)
 
+    def test_getMedian_even(self):
+        myList = [ 4, 5, 6, 7,10, 45]
+        handlerStatistics = myStatistics.myStatistics()
+        result = handlerStatistics.getMedian(myList)
+        self.assertEqual(result, 6.5)
+
+    def test_getMedian_odd(self):
+        myList = [ 4, 35, 5, 6, 7,10, 45]
+        handlerStatistics = myStatistics.myStatistics()
+        result = handlerStatistics.getMedian(myList)
+        self.assertEqual(result, 7)
+
+    def test_getMedian_one(self):
+        myList = [4]
+        handlerStatistics = myStatistics.myStatistics()
+        result = handlerStatistics.getMedian(myList)
+        self.assertEqual(result, 4)
+
+    def test_getMedian_empty(self):
+        myList = []
+        handlerStatistics = myStatistics.myStatistics()
+        result = handlerStatistics.getMedian(myList)
+        self.assertIsNone(result)
+
+    def test_getNQuartil_noQ(self):
+        myList = [ 4, 35, 5, 6, 7,10, 45, 34,32, 79, 4, 35, 5, 6, 7,10, 45, 34,60, 79, 4, 35, 5, 56, 7,40, 65, 43,32, 79]
+        handlerStatistics = myStatistics.myStatistics()
+        n = 1
+        result = handlerStatistics.getNquartil(n, myList)
+        self.assertEquals(result,6)
+
+    def test_getNQuartil_Qint(self):
+        myList = [56, 7,40, 65, 43,32, 79]
+        handlerStatistics = myStatistics.myStatistics()
+        n = 3
+        result = handlerStatistics.getNquartil(n, myList)
+        self.assertEquals(result,65)
+
+    def test_getNPercentil_PNoInt(self):
+        myList = [4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 37, 120, 245, 34, 32, 79, 4, 35, 5, 643, 7, 10, 45,
+                  34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 749, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7,
+                  10, 45, 10, 45, 34, 32, 79,
+                  35, 678, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 7, 10,
+                  45, 34, 32, 79, 47, 10, 45, 34, 32, 79, 4, 35, 5, 790, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35,
+                  5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79]
+        handlerStatistics = myStatistics.myStatistics()
+        n = 150
+        result = handlerStatistics.getNpercentil(n, myList)
+        self.assertIsNone(result)
+
+    def test_getNPercentil_PInt(self):
+        myList = [4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 37, 120, 245, 34, 32, 79,4, 35, 5, 643, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 749, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79,
+                 35, 678, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79,4, 35, 5, 6, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 7, 10, 45, 34, 32, 79,4, 35, 5, 6, 7, 10, 45, 34, 32, 79,4, 35, 5, 6, 7, 10, 45, 34, 32, 79,4, 35, 5, 6, 7, 10, 45, 34, 32, 79,5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 27, 150, 455, 34, 32, 79, 4, 35, 5, 6, 75, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7, 45, 34, 32, 790, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79]
+        handlerStatistics = myStatistics.myStatistics()
+        n = 75
+        result = handlerStatistics.getNpercentil(n, myList)
+        self.assertEquals(result, 35)
+
+    def test_getNPercentil_PNoInt(self):
+        myList = [4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 37, 120, 245, 34, 32, 79, 4, 35, 5, 643, 7, 10, 45,
+                  34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 749, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7,
+                  10, 45, 10, 45, 34, 32, 79,
+                  35, 678, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 7, 10,
+                  45, 34, 32, 79, 47, 10, 45, 34, 32, 79, 4, 35, 5, 790, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79, 4, 35,
+                  5, 6, 7, 10, 45, 34, 32, 79, 4, 35, 5, 6, 7, 10, 45, 34, 32, 79]
+        handlerStatistics = myStatistics.myStatistics()
+        n = 50
+        result = handlerStatistics.getNpercentil(n, myList)
+        self.assertEquals(result, 32)
 
 if __name__ == '__main__':
     unittest.main()
